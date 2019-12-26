2119ec1bc39e239232bf851b5715d1b95b036027
ttwkr <ttwkor@naver.com>
2019-12-26 19:40:39 +0900

    account에 Hosts model 추가

--- account/models.py
+++ account/models.py
@@ -9,3 +9,13 @@ class Accounts(models.Model):
 
     class Meta:
         db_table = 'accounts'
+
+class Hosts(models.Model):
+    nick_name  = models.CharField(max_length = 50)
+    email      = models.CharField(max_length = 200, unique = True)
+    password   = models.CharField(max_length = 300)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'hosts'
