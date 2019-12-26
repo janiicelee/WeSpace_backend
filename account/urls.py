454ab8ba3153b7a4948a787d861226613a611b6d
meekukin <meekukin@gmail.com>
2019-12-26 21:21:18 +0900

    account - login api

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views      import AccountsView
+from .views      import AccountsView, AuthView
 urlpatterns = [
-     path('', AccountsView.as_view() )
+     path('', AccountsView.as_view()),
+     path('/auth', AuthView.as_view())
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 import json
 import bcrypt
+import jwt
 
 from django.views           import View
 from django.http            import JsonResponse
@@ -40,4 +41,31 @@ class AccountsView(View):
         except ValidationError:
             return JsonResponse({'message':'NOT_AN_EMAIL'}, status = 400)
 
-        
+class AuthView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            if len(data['password']) < 6 :
+                return JsonResponse({'message':'TOO_SHORT'}, status = 400)
+
+            user = Accounts.objects.get(email = data['email'])
+            if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
+                access_token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm='HS256')
+                return JsonResponse({'access_token':access_token.decode('utf-8')}, status=200)
+            else:
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status = 401)
+
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+
+        except Accounts.DoesNotExist:
+            return JsonResponse({'message':'INVALID_ACCOUNT'}, status = 400)
+
+        except TypeError:
+            return JsonResponse({'message':'WRONG_INPUT_VALUE'}, status = 400)
+
+        except ValidationError:
+            return JsonResponse({'message':'NOT_AN_EMAIL'}, status = 400)
+
+
