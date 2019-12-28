49de11c2ea6347cf88de08e00e159fc09bc3461e
ttwkr <ttwkor@naver.com>
2019-12-28 15:28:28 +0900

    like(찜) model 추가

--- account/models.py
+++ account/models.py
@@ -1,21 +1,36 @@
 from django.db import models
 
+
 class Accounts(models.Model):
-    nick_name  = models.CharField(max_length = 50)
-    email      = models.CharField(max_length = 200, unique = True)
-    password   = models.CharField(max_length = 300)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    nick_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'accounts'
 
+
 class Hosts(models.Model):
-    nick_name  = models.CharField(max_length = 50)
-    email      = models.CharField(max_length = 200, unique = True)
-    password   = models.CharField(max_length = 300)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    nick_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'hosts'
+
+
+class Likes(models.Model):
+
+    from space.models import Spaces
+
+    user = models.ForeignKey(Accounts, on_delete=models.SET_NULL, null=True)
+    space = models.ForeignKey(Spaces, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'likes'
