abe2e572e482737ed8a5dba9133ff07f4873d40b
ttwkr <ttwkor@naver.com>
2019-12-30 09:10:14 +0900

    reservation model 추가

--- account/models.py
+++ account/models.py
@@ -1,21 +1,37 @@
 from django.db import models
 
+
 class Accounts(models.Model):
-    nick_name  = models.CharField(max_length = 50)
-    email      = models.CharField(max_length = 200, unique = True)
-    password   = models.CharField(max_length = 300)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    nick_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'accounts'
 
+
 class Hosts(models.Model):
-    nick_name  = models.CharField(max_length = 50)
-    email      = models.CharField(max_length = 200, unique = True)
-    password   = models.CharField(max_length = 300)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    nick_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'hosts'
+
+
+class Reservations(models.Model):
+    user = models.ForeignKey(
+        'Accounts', on_delete=models.SET_NULL, null=True)
+    confirm = models.BooleanField(null=True)
+    space = models.ForeignKey(
+        'space.Spaces', on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey('Hosts', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'reservations'
