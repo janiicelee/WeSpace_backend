da801090630d64dcd811f8077106b36d7cd62b0f
ttwkr <ttwkor@naver.com>
2020-01-01 22:23:33 +0900

    기존에 있던 views코드를 제거

--- space/views.py
+++ space/views.py
@@ -12,24 +12,3 @@ class CategoryView(View):
             return JsonResponse({'category': list(category)}, status=200)
         except:
             return JsonResponse({'result': 'error'}, status=400)
-
-
-class DetailSpaceView(View):
-    def get(self, request, space_id):
-        # try:
-        arr = []
-        space_image = Spaces.objects.prefetch_related(
-            'images_set').get(id=space_id)
-        space = Spaces.objects.filter(id=space_id).values(
-            'title', 'short_intro', 'price', 'long_intro', 'open_time', 'close_time')
-        host = Hosts.objects.filter(id=space.values()[0]['host_id'])
-        for i in space_image.images_set.all().values():
-            arr.append(i['space_image'])
-        return JsonResponse(
-            {
-                'space': list(space),
-                'host': list(host.values('nick_name', 'email')),
-                'image': arr
-            })
-        # except:
-        #     return JsonResponse({'result': 'error'})
