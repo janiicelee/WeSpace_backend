1a822a80cbb0e5f5d51ae2dd2e940d0fc4ae1a8f
ttwkr <ttwkor@naver.com>
2020-01-01 20:10:07 +0900

    Space model에 최소인원 및 최소시간 컬럼 추가

--- space/views.py
+++ space/views.py
@@ -1,7 +1,7 @@
 from django.views import View
 from django.http import JsonResponse
 
-from .models import Spaces, Space_Categories
+from .models import *
 
 
 class CategoryView(View):
@@ -12,3 +12,24 @@ class CategoryView(View):
             return JsonResponse({'category': list(category)}, status=200)
         except:
             return JsonResponse({'result': 'error'}, status=400)
+
+
+class DetailSpaceView(View):
+    def get(self, request, space_id):
+        # try:
+        arr = []
+        space_image = Spaces.objects.prefetch_related(
+            'images_set').get(id=space_id)
+        space = Spaces.objects.filter(id=space_id).values(
+            'title', 'short_intro', 'price', 'long_intro', 'open_time', 'close_time')
+        host = Hosts.objects.filter(id=space.values()[0]['host_id'])
+        for i in space_image.images_set.all().values():
+            arr.append(i['space_image'])
+        return JsonResponse(
+            {
+                'space': list(space),
+                'host': list(host.values('nick_name', 'email')),
+                'image': arr
+            })
+        # except:
+        #     return JsonResponse({'result': 'error'})
