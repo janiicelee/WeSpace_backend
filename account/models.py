1c27e56bce73f53c9b9d1870dc33eed545c81e9d
Yerin Lee <57986882+meekukin@users.noreply.github.com>
2020-03-12 16:27:23 +0900

    Update models.py

--- account/models.py
+++ account/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Account(models.Model):
 
-    nick_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=300)
-    like = models.ManyToManyField('space.Spaces', through='Likes')
-    nick_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=300)
+    nick_name  = models.CharField(max_length=50)
+    email      = models.CharField(max_length=200, unique=True)
+    password   = models.CharField(max_length=300)
+    like       = models.ManyToManyField('space.Space', through='Like')
+    nick_name  = models.CharField(max_length=50)
+    email      = models.CharField(max_length=200, unique=True)
+    password   = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -18,28 +18,26 @@ class Account(models.Model):
 
 
 class Host(models.Model):
-    nick_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=300)
+    nick_name   = models.CharField(max_length=50)
+    email       = models.CharField(max_length=200, unique=True)
+    password    = models.CharField(max_length=300)
     phonenumber = models.CharField(max_length=11, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'hosts'
 
 
 class Reservation(models.Model):
-    user = models.ForeignKey(
-        'Accounts', on_delete=models.SET_NULL, null=True)
-    confirm = models.BooleanField(null=True)
-    space = models.ForeignKey(
-        'space.Spaces', on_delete=models.SET_NULL, null=True)
-    host = models.ForeignKey('Hosts', on_delete=models.SET_NULL, null=True)
-    year = models.CharField(max_length=10)
-    month = models.CharField(max_length=10)
-    day = models.CharField(max_length=10)
-    hour = models.CharField(max_length=300)
+    user       = models.ForeignKey('Account', on_delete=models.SET_NULL, null=True)
+    confirm    = models.BooleanField(null=True)
+    space      = models.ForeignKey('space.Space', on_delete=models.SET_NULL, null=True)
+    host       = models.ForeignKey('Host', on_delete=models.SET_NULL, null=True)
+    year       = models.CharField(max_length=10)
+    month      = models.CharField(max_length=10)
+    day        = models.CharField(max_length=10)
+    hour       = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -48,9 +46,8 @@ class Reservation(models.Model):
 
 
 class Like(models.Model):
-    user = models.ForeignKey('Accounts', on_delete=models.SET_NULL, null=True)
-    space = models.ForeignKey(
-        'space.Spaces', on_delete=models.SET_NULL, null=True)
+    user       = models.ForeignKey('Account', on_delete=models.SET_NULL, null=True)
+    space      = models.ForeignKey('space.Space', on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
