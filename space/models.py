7cb0aa5b12de3314349cd6ba5c819e3deb87ba8b
Yerin Lee <57986882+meekukin@users.noreply.github.com>
2020-03-12 16:24:08 +0900

    Update models.py

--- space/models.py
+++ space/models.py
@@ -1,108 +1,102 @@
-from django.db import models
-from account.models import Hosts, Accounts
+from django.db      import models
+from account.models import Host, Account
 
 
 class Space(models.Model):
-    title = models.CharField(max_length=100)
-    short_intro = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    long_intro = models.TextField()
-    location = models.CharField(max_length=300)
-    host = models.ForeignKey(
-        'account.Hosts', on_delete=models.SET_NULL, null=True)
-    min_time = models.CharField(max_length=10, null=True)
-    min_guest = models.CharField(max_length=10, null=True)
-    open_time = models.CharField(max_length=10)
-    close_time = models.CharField(max_length=10)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    amenity_space = models.ManyToManyField(
-        'Amenities', through='Space_Amenities')
+    title         = models.CharField(max_length=100)
+    short_intro   = models.CharField(max_length=200)
+    price         = models.DecimalField(max_digits=12, decimal_places=2)
+    long_intro    = models.TextField()
+    location      = models.CharField(max_length=300)
+    host          = models.ForeignKey('account.Host', on_delete=models.SET_NULL, null=True)
+    min_time      = models.CharField(max_length=10, null=True)
+    min_guest     = models.CharField(max_length=10, null=True)
+    open_time     = models.CharField(max_length=10)
+    close_time    = models.CharField(max_length=10)
+    create_at     = models.DateTimeField(auto_now_add=True)
+    update_at     = models.DateTimeField(auto_now=True)
+    amenity_space = models.ManyToManyField('Amenity', through='SpaceAmenity')
 
     class Meta:
         db_table = 'spaces'
 
 
 class Amenity(models.Model):
-    amenity = models.CharField(max_length=300)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    amenity    = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'amenities'
 
 
 class Holiday(models.Model):
-    holiday = models.DateTimeField()
-    space = models.ForeignKey('Spaces', on_delete=models.SET_NULL, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    holiday    = models.DateTimeField()
+    space      = models.ForeignKey('Spaces', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'holiday'
+        db_table = 'holidays'
 
 
 class SpaceCategory(models.Model):
-    category = models.CharField(max_length=200)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    category   = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'space_categories'
 
 
 class Notice(models.Model):
-    notice = models.CharField(max_length=300)
-    space = models.ForeignKey('Spaces', on_delete=models.SET_NULL, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    notice     = models.CharField(max_length=300)
+    space      = models.ForeignKey('Spaces', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'notices'
 
 
 class SpaceAmenity(models.Model):
-    amenity = models.ForeignKey(
-        'Amenities', on_delete=models.SET_NULL, null=True)
-    space = models.ForeignKey(
-        'Spaces', on_delete=models.SET_NULL, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    amenity    = models.ForeignKey('Amenities', on_delete=models.SET_NULL, null=True)
+    space      = models.ForeignKey('Spaces', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'space_amenities'
 
 
 class Question(models.Model):
-    space = models.ForeignKey(
-        'Spaces', on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(
-        'account.Accounts', on_delete=models.SET_NULL, null=True)
-    content = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    space      = models.ForeignKey('Space', on_delete=models.SET_NULL, null=True)
+    user       = models.ForeignKey('account.Account', on_delete=models.SET_NULL, null=True)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Mata:
         db_table = 'questions'
 
 
 class Review(models.Model):
-    user = models.ForeignKey(Accounts, on_delete=models.SET_NULL, null=True)
-    space = models.ForeignKey(Spaces, on_delete=models.SET_NULL, null=True)
-    content = models.TextField()
-    image = models.URLField(max_length=2500)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    user       = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    space      = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True)
+    content    = models.TextField()
+    image      = models.URLField(max_length=2500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'reviews'
 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=30)
-    space = models.ForeignKey(Spaces, on_delete=models.SET_NULL, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    tag        = models.CharField(max_length=30)
+    space      = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'tags'
@@ -110,9 +104,9 @@ class Tag(models.Model):
 
 class Image(models.Model):
     space_image = models.URLField(max_length=2500)
-    space = models.ForeignKey(Spaces, on_delete=models.SET_NULL, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    space       = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'images'
